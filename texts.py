c19b43edcc22e56ff152dcf7312c959bf54bd012
Luomj0503 <127477311+Luomj0503@users.noreply.github.com>
2023-03-13 21:15:58 +0800

    Add files via upload

---
+++ texts.py
@@ -0,0 +1,89 @@
+from PIL import Image
+
+
+class Texts:
+    def __init__(_self):
+        pass
+
+    def text1(_self):
+        TEXT1 = """
+        <body style='text-align: justify; color: black;'>
+        <p> general description based on Machine Learning models using molecular fingerprints.It is only necessary to inform the SMILES or CAS Number format of a specific molecule.
+        The models present in this work were built using <a href="https://scikit-learn.org/stable/"target="_blank">scikit-learn</a> and <a href="https://xgboost.readthedocs.io/en/latest/python/python_api.html"target="_blank">XGBoost</a> packages.
+        The predict values can be confronted with experimental values available in our work . 
+        </p>
+        </body>         
+        """
+
+        return TEXT1
+
+    def text1_2(_self):
+        TEX1_2 = """
+        ###  Prediction of S-ZVI Reaction Rate with Organic Pollutants by Machine Learning
+        <body style='text-align: justify; color: black;'>
+        <p>
+        Data from the literature, structured and with molecular names converted by SMILES provide molecular fingerprints (MF). Besides, independent variables include
+        pH, temperature, S-ZVI concentration and pollutant concentration. While rate constants are the dependent variables of our model developed implemented in six 
+        machine learning algorithms – Gradient Boosted Regression Tree(GBRT), Desicion Tree(DT), Support Vector Regression(SVR),XGboost (XBG), Random Forest (RF) 
+        and Neural Network (NN).We found that the Neural Network and Gradient Boosted Regression Tree made the best predictions, then interpreted the model by SHAP.
+        </p>
+        </body>
+        """
+
+        return TEX1_2
+
+    def text1_3(_self):
+        TEX1_3 = """
+        ###  Prediction of O3 Reaction Rate with Organic Pollutants by Machine Learning
+        <body style='text-align: justify; color: black;'>
+        <p>
+        The process is basically the same as before,but we found that the Neural Network and XGboost made the best predictions.
+        </p>
+        </body>
+        """
+
+        return TEX1_3
+
+    def text1_4(_self):
+        TEXT1_4 = """
+        <body style='text-align: justify; color: black;'>
+        <h1 style='text-align: justify; color: black;'>pySiRC website</h1>
+        <p> pySiRC - a program that permits compilation of kinetic parameters in aqueous and atmospheric environment.
+        <h2 style='text-align: justify; color: black;'>Citation</h2>
+        Please cite the following reference when publishing results obtained with pySiRC:<br>
+        (i) Sanches-Neto, F. O., Dias-Silva, J. R., Keng Queiroz Junior, L. H., & Carvalho-Silva, V. H. (2021). “pySiRC”: Machine Learning Combined with Molecular Fingerprints to Predict the Reaction Rate Constant of the Radical-Based Oxidation Processes of Aqueous Organic Contaminants. Environmental Science & Technology, 55(18), 12437-12448, <a href="https://doi.org/10.1021/acs.est.1c04326"target="_blank">https://doi.org/10.1021/acs.est.1c04326</a><br>
+        (ii) Sanches-Neto, F. O., Dias-Silva, J. R., de Oliveira, V.M,  Aquilanti, V., & Carvalho-Silva, V. H. (2022). Evaluating and elucidating the reactivity of OH radicals with atmospheric organic pollutants: Reaction kinetics and mechanisms by machine learning. Atmospheric Environment, 119019, <a href="https://doi.org/10.1016/j.atmosenv.2022.119019"target="_blank">https://doi.org/10.1016/j.atmosenv.2022.119019</a>
+        </p>
+        </body>
+        """
+        return TEXT1_4
+
+    def text2(_self):
+        TEXT2 = """
+        This web platform is free and open source and you are very welcome to contribute.
+        This Application has GNU GPL license. All source code can be accessed [here](https://github.com/Mphyschem/pySiRC).
+        """
+
+        return TEXT2
+
+    def text3(_self):
+        TEXT3 = """
+        <body style='text-align: justify; color: black;'>
+        <h2 style='text-align: justify; color: black;'>About</h2>
+        <p>
+        pySiRC was designed to facilitate the automatic researchers to prediction and analyze of reaction rate constants
+        using Machine Learning models, since the theoretical calculation and experimental measures of these kinetic
+        parameters can be extremely challenging. The tools are designed to be quick and simple users with few clicks
+        can make the prediction. The tools are rigorous enough to have applications in academic research.
+        All the source code for this application and the steps for local installation are at <a href="https://github.com/Mphyschem/pySiRC"target="_blank">GitHub</a>.
+        You are free to use, change and distribute this application under the GNU GPL license. The maintainers of this
+        package so far are: Flávio Olimpio Sanches Neto (flavio_olimpio@outlook.com), Jefferson Richard Dias (jrichardquimica@gmail.com),
+        and Vitor Mendes (vitor.mendes.ag@gmail.com).
+
+        <h2 style='text-align: justify; color: black;'>Supporters</h2>
+        </p>
+        </body>
+        """
+
+        return TEXT3
+
